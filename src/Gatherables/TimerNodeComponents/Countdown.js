fd35e8c21983b36c9acbb6695d80ae207256282d
Indanaiya <14353902+Indanaiya@users.noreply.github.com>
2020-09-18 12:17:10 +0100

    Add comments and fix an if satement.

--- src/Gatherables/TimerNodeComponents/Countdown.js
+++ src/Gatherables/TimerNodeComponents/Countdown.js
@@ -4,29 +4,43 @@ import { timer } from "../../App.js";
 
 /**
  * A component to display a timer counting down to a specified time
+ * Props: targetTime - The time being counted down to
+ *  countdownComplete - A callback to be executed when the targetTime is reached
  */
 export default class Countdown extends React.Component {
+  /**
+   * Initialises the state
+   */
   constructor() {
     super();
     this.state = {};
   }
 
+  /**
+   * Subscribes to a timer to update the time every second
+   */
   componentDidMount() {
     timer.subscribe(this, () => {
       const time = new Date(this.props.targetTime - Date.now());
       if (time.getTime() <= 0) {
-        console.log("hi");
         this.props.countdownComplete();
         this.setState({ time: 0 });
+      } else {
+        this.setState({ time });
       }
-      this.setState({ time });
     });
   }
 
+  /**
+   * Unsubscribes from the timer
+   */
   componentWillUnmount() {
     timer.unsubscribe(this);
   }
 
+  /**
+   * Displays the time stored in this.state.time in the format HH:MM
+   */
   render() {
     const { time } = this.state;
     return (
